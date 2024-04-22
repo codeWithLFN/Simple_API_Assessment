7a9ff54fd7388cc8a0638f45a79924a16bee1548
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-04-22 19:54:17 +0200

    Adding controller

---
+++ Simple_API_Assessment/Controllers/ApplicantController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_API_Assessment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicantController : ControllerBase
+    {
+    }
+}
