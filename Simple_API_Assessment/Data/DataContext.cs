22ef5745bc1f042e0b139726177a701ce0490b22
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-04-28 01:57:02 +0200

    Seed data

--- Simple_API_Assessment/Data/DataContext.cs
+++ Simple_API_Assessment/Data/DataContext.cs
@@ -12,7 +12,26 @@ namespace Simple_API_Assessment.Data
         public DbSet<Applicant> Applicants { get; set; }
         public DbSet<Skill> Skills { get; set; }
 
+        // Seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Applicant>().HasData(
+                new Applicant { Id = 1, Name = "Lufuno Nemudivhadi" },
+                new Applicant { Id = 2, Name = "Ofentse Sithole" }
+            );
 
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "C#" },
+                new Skill { Id = 2, Name = "Java" },
+                new Skill { Id = 3, Name = "Python" }
+            );
 
+            // Configure the relationship between Applicant and Skill
+            modelBuilder.Entity<Skill>()
+                .HasOne(s => s.Applicant)
+                .WithMany(a => a.Skills)
+                .HasForeignKey(s => s.ApplicantId);
+
+        }
     }
 }
