83e4732e75f64b62dbb959f628f1071901e7a0b4
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-04-28 02:10:51 +0200

    Repository

---
+++ Simple_API_Assessment/Data/Repository/ApplicantRepo.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Simple_API_Assessment.Models;
+
+namespace Simple_API_Assessment.Data.Repository
+{
+    public class ApplicantRepo : IApplicantRepository
+    {
+        private readonly DataContext _context;
+
+        public ApplicantRepo(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Applicant>> GetAllApplicantsAsync()
+        {
+            return await _context.Applicants.Include(a => a.Skills).ToListAsync();
+        }
+
+        public async Task<Applicant> GetApplicantAsync(int id)
+        {
+            return await _context.Applicants.Include(a => a.Skills).FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task CreateApplicantAsync(Applicant applicant)
+        {
+            _context.Applicants.Add(applicant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateApplicantAsync(Applicant applicant)
+        {
+            _context.Applicants.Update(applicant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteApplicantAsync(int id)
+        {
+            var applicant = await _context.Applicants.FindAsync(id);
+            if (applicant != null)
+            {
+                _context.Applicants.Remove(applicant);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Simple_API_Assessment/Data/Repository/IApplicantRepository.cs
@@ -0,0 +1,14 @@
+﻿using Simple_API_Assessment.Models;
+
+namespace Simple_API_Assessment.Data.Repository
+{
+    public interface IApplicantRepository
+    {
+        // Define the methods that will be implemented in the ApplicantRepo class
+        Task<List<Applicant>> GetAllApplicantsAsync();
+        Task<Applicant> GetApplicantAsync(int id);
+        Task CreateApplicantAsync(Applicant applicant);
+        Task UpdateApplicantAsync(Applicant applicant);
+        Task DeleteApplicantAsync(int id);
+    }
+}
